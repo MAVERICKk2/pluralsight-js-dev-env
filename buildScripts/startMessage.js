135c9d563fd460352d6026b2f1d95c227b92df3b
Uriel López <maverick@MAVERICKs-iMac.local>
2017-08-28 22:22:41 -0500

    added eslint and webpack to my project

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,6 @@
 //var chalk = require("chalk");
 import chalk from "chalk"; //ES6 style being transpiling by Babel
 
+/* eslint-disable no-console */
+
 console.log(chalk.green("Starting app in dev mode..."));
